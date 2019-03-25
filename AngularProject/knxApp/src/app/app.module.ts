f8440e71d21251822587ac11c5ba50ce4945a557
Thibault <thibault.leroy.1@etudiant.univ-rennes1.fr>
2019-03-25 19:41:56 +0100

    ok

--- AngularProject/knxApp/src/app/app.module.ts
+++ AngularProject/knxApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgxMqttClientModule} from 'ngx-mqtt-client';
+import { NgxMqttClientModule } from 'ngx-mqtt-client';
 import { AppComponent } from './app.component';
 import { OnsenModule } from 'ngx-onsenui';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -11,7 +11,7 @@ import { MainComponent } from './main/main.component';
 @NgModule({
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
-],
+  ],
   declarations: [
     AppComponent,
     HomeComponent,
@@ -24,8 +24,9 @@ import { MainComponent } from './main/main.component';
     NgxMqttClientModule.withOptions({
       host: '3.83.149.37',
       protocol: 'ws',
-      port: 9001
-  })
+      port: 9001,
+      clientId: 'thib'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
